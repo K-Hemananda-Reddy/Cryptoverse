24faa75630fc4baa369d3bcd58e06d9ef24132ae
Asaduddin aman <83344388+K-Hemananda-Reddy@users.noreply.github.com>
2023-07-26 10:34:54 +0530

    changed layout

--- components/CryptoCurrencies.js
+++ components/CryptoCurrencies.js
@@ -24,8 +24,8 @@ const CryptoCurrencies = ({ simplified }) => {
     <div>
       {!simplified &&(
 
-        <div>
-        <input onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search Cryptocurrency" />
+        <div className="flex justify-center my-4">
+        <input className="p-2 border md:w-1/3 w-10/12 border-blue-500 border-2 rounded-xl text-center" onChange={(e)=>setSearch(e.target.value)} type="text" placeholder="Search Cryptocurrency" />
       </div>
         )}
       <div className="grid mt-4 grid-cols-1 md:grid-cols-3 gap-4 ">
