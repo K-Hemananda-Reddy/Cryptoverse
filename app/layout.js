7e8603ab68b9042454a772dac3253e471e19cf7b
Hemananda <hemanandreddy3@gmail.com>
2023-11-30 13:22:55 +0530

    commit latest

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Provider } from 'react-redux'
 import store from '@/store'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
+      <Script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9716041141351749' crossOrigin='anonymous'/>
 
     <html lang="en">
       <body className={inter.className}>
