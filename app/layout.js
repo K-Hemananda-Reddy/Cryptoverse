bdfcc025b7184b070a7b9f911171377772d34b86
Hemananda <hemanandreddy3@gmail.com>
2023-11-30 13:27:35 +0530

    latest commit 2

--- app/layout.js
+++ app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <Provider store={store}>
       <Script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9716041141351749' crossOrigin='anonymous'/>
-
+      <Script async src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9716041141351749' crossOrigin='anonymous'/>
     <html lang="en">
       <body className={inter.className}>
 
